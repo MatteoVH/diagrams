10c40b1c5d23a6cd3386c9d03bb308a639c280e6
Matteo Home <matteohome@matteo-macbook.local>
2017-08-26 21:00:08 -0400

    create points and link

--- Diagram.hs
+++ Diagram.hs
@@ -2,10 +2,33 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE TypeFamilies              #-}
 
-import Diagrams.Prelude
+import System.Random
+import Control.Monad
+import Diagrams.TwoD
+import Diagrams.Prelude hiding (Point)
 import Diagrams.Backend.SVG.CmdLine
+import Data.List
 
-myCircle :: Diagram B
-myCircle = circle 1
+type Point = P2 Double
 
-main = mainWith myCircle
+generatePoint :: IO Point
+generatePoint = do
+  x <- randomIO :: IO Double
+  y <- randomIO :: IO Double
+  return (p2 (x, y))
+
+cartesianProduct :: [a] -> [(a, a)]
+cartesianProduct list =
+  case list of
+    [] -> []
+    head : rest -> map (\ a -> (head, a)) rest ++ cartesianProduct rest
+
+createLines :: (Point, Point) -> Diagram B
+createLines (a, b) =
+  fromVertices ([a, b])
+
+main :: IO ()
+main = do
+  points <- replicateM 50 generatePoint
+  mainWith ((mconcat (map createLines (cartesianProduct points))) # lw 0.1)
+  putStrLn "use double quotes"
